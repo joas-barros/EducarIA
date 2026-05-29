7dc40fbf8d000993a982f8ef6c88172dc03bdc89
Joás Gabriel Barros de Sousa <118216212+joas-barros@users.noreply.github.com>
2026-05-28 21:56:51 -0300

    feat: configure media file storage for ementa uploads

--- educaria/settings.py
+++ educaria/settings.py
@@ -118,6 +118,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
