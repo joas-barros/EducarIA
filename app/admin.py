b9cd4aac913578b22ad98e303de2bfbf27a70811
Clara Letícia <claralet1346@gmail.com>
2026-05-28 21:39:14 -0300

    feat: infra

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Professor, Disciplina
 
-# Register your models here.
+admin.site.register(Professor)
+admin.site.register(Disciplina)
--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('auth/logout/', views.logout_view, name='logout'),
     path('auth/cadastro/', views.CadastroWizardView.as_view(views.CADASTRO_FORMS), name='cadastro'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+
+    path('disciplinas/', views.disciplinas_list, name='disciplinas'),
+    path('disciplinas/nova/', views.DisciplinaCreateView.as_view(views.DISCIPLINA_FORMS), name='disciplina_nova'),
+    path('disciplinas/<uuid:pk>/', views.disciplina_detalhe, name='disciplina_detalhe'),
+    path('disciplinas/<uuid:pk>/editar/', views.disciplina_editar, name='disciplina_editar'),
+    path('disciplinas/<uuid:pk>/excluir/', views.disciplina_excluir, name='disciplina_excluir'),
 ]
